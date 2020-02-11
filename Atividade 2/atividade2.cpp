53479534379a07770fddbbb41388df494b5f7275
Tales Tomaz Alves <54077178+tta13@users.noreply.github.com>
2020-02-10 23:31:28 -0300

    Comentando a atividade 2

--- Atividade 2/atividade2.cpp
+++ Atividade 2/atividade2.cpp
@@ -1,30 +1,31 @@
 #include <cstdio>
+#include <iostream>
 using namespace std;
 
 int main() {
     int a, b;
     while(scanf("%d %d", &a, &b) == 2){
-        int countA = 0, countB = 0;
-        if(((2*a) + 2) < b || ((2*b) + 2) < a)
-            printf("Impossível encontrar sequencia");
+        int countA = 0, countB = 0;//usados para contar quantas vezes seguidas escrevi determinado caractere
+        if(((2*a) + 2) < b || ((2*b) + 2) < a)//caso o numero de um seja maior que 2 vezes o numero do outro mais 2, é impossivel
+            cout << "Impossível encontrar sequencia";
         else{
             while(a != 0 || b != 0){
-                int dif = b - a;
+                int dif = b - a;//computa a diferença entre os dois
                 if((dif > 0 || countA == 2) && (countB < 2)){
-                    printf("b");
-                    b--;
+                    cout << 'b';
+                    b--; //diminui a quantidade de b's disponiveis
                     countB++;
                     countA = 0;
                 }
                 else{
-                    printf("a");
-                    a--;
+                    cout << 'a';
+                    a--; //diminui os a's
                     countA++;
                     countB = 0;
                 }
             }
         }
-        printf("\n");
+        cout << endl;
     }
 	return 0;
 }
